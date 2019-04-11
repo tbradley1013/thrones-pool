68584c739853f5850914538613349f2e2dfcf038
Tyler Bradley <tyler.bradley@phila.gov>
2019-04-11 13:05:23 -0400

    moved data processing to separate script to save and then read within the shiny app

--- global.R
+++ global.R
@@ -17,59 +17,8 @@ suppressWarnings({
 })
 
 
-read_submission <- function(file){
-  # browser()
-  char_locs <- c("B4:C24", "D4:E24")
-  
-  name <- read_excel(file, range = "C2", col_names = FALSE)
-  name <- name[[1,1]]
-  
-  character_df <- map_dfr(char_locs, ~{
-    df <- read_excel(file, range = .x)
-    
-    df <- df %>% 
-      janitor::clean_names()
-    
-    return(df)
-  })
-  
-  character_df <- character_df %>% 
-    mutate(
-      question_type = "character"
-    ) %>% 
-    select(question = character, question_type, answer = lives_dies)
-  
-  
-  props_df <- read_excel(file, range = "B25:E34") %>% 
-    janitor::clean_names() %>% 
-    select(bonus_questions, answer) %>% 
-    mutate(
-      points = str_extract(bonus_questions, "\\([0-9]") %>% str_extract("[0-9]") %>% as.numeric(),
-      question = str_extract(bonus_questions, "(^.*)?\\?"),
-      question_type = "prop"
-    ) %>% 
-    select(question, question_type, answer, points)
-  
-  output <- character_df %>% 
-    bind_rows(props_df) %>% 
-    mutate(submitter = name) %>% 
-    select(submitter, everything())
-  
-  return(output)
-  
-}
-
-
-parse_submissions <- function(){
-  submissions <- list.files("submissions", full.names = TRUE, recursive = TRUE)
-  
-  map_dfr(submissions, read_submission)
-}
-
-
-submissions <- parse_submissions()
-
-master <- read_submission("answer-key.xlsx")
+submissions <- read_rds("data/submissions.rds")
+master <- read_rds("data/master.rds")
 
 
 answers <- submissions %>% 
