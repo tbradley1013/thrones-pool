8efd4a77e36bd6e31c277859039fd15c25c6d228
Tyler Bradley <tyler.bradley@phila.gov>
2019-04-29 08:35:22 -0400

    updated "last update"

--- ui.R
+++ ui.R
@@ -77,7 +77,7 @@ shinyUI(
       ),
       inputs = div(
         p(
-          "Last Updated: S8E2", 
+          "Last Updated: S8E3", 
           style = "color:white;font-size:10px;"
         ),
         style = "position:relative;float:right;"
